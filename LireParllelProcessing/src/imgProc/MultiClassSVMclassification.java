0cb26fdb71edf6e8e944f3186bef1786f1299ab3
ishan <ipatwa@ufl.edu>
2014-09-23 01:05:23 -0400

    structure update

--- LireParllelProcessing/src/imgProc/MultiClassSVMclassification.java
+++ LireParllelProcessing/src/imgProc/MultiClassSVMclassification.java
@@ -5,11 +5,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.imgscalr.Scalr;
 
 import net.semanticmetadata.lire.clustering.Cluster;
@@ -32,7 +39,7 @@ public class MultiClassSVMclassification {
 	public static ArrayList<Integer[]> quantized_images = new ArrayList<>();
 	private static MserSiftFeatureOperations mserFileOperations = new MserSiftFeatureOperations("");
 	private static KmeansClustering kmeans = new KmeansClustering();
-
+	private static Map<Integer,Map<Integer,Integer> >  trainDataSVM =null;
 	
 	public static void main(String[] args) throws IOException {
 		mserFileOperations._init_("");
@@ -43,7 +50,7 @@ public class MultiClassSVMclassification {
 
 	public static void _init_(String train_images) throws IOException{	
 		//handle a image at a time , get the image , extract the features , quantize the features.
-
+		int label = 0;
 		File dir = new File(train_images);
 		File[] directoryListing = dir.listFiles();
 		System.out.println("directory size:"+directoryListing.length);
@@ -59,12 +66,13 @@ public class MultiClassSVMclassification {
 					if ( img.getTileHeight() < 64 || img.getTileWidth() < 64  ){
 						img = Scalr.resize(img, Scalr.Method.AUTOMATIC, 100, null);
 					}
-					//System.out.println("fetching training features"+child.getAbsolutePath());
+					System.out.println("fetching training features"+child.getAbsolutePath());
 					List<Feature> temp_features = MserSiftParallel.getSiftMserFeatures(img);
-					quantizeFeatures(temp_features);
+					quantizeFeatures(temp_features,label);
 					temp_features.clear();
 					img.flush();
 				}else {
+					label++;
 					File[] newfiles = child.listFiles();
 					for (File file : newfiles) { 			    	  		    	
 						BufferedImage img = ImageIO.read(file);   		    	
@@ -73,28 +81,29 @@ public class MultiClassSVMclassification {
 						if ( img.getTileHeight() < 64 || img.getTileWidth() < 64  ){
 							img = Scalr.resize(img, Scalr.Method.AUTOMATIC, 100, null);
 						}		    		    	
-					//	System.out.println("fetching features for "+file.getAbsolutePath());
+						System.out.println("fetching features for "+file.getAbsolutePath());
 						List<Feature> temp_features = MserSiftParallel.getSiftMserFeatures(img);
-						quantizeFeatures(temp_features);
+						quantizeFeatures(temp_features,label);
 						temp_features.clear();
 						img.flush();
 					}
 				}}}
-		
+		writeLibSvmDataToFile();
 	}
 
-	public static void 	quantizeFeatures(List<Feature> features) throws IOException {
+	public static void 	quantizeFeatures(List<Feature> features,Integer label) throws IOException {
 				ArrayList<double[]>  descriptors = new ArrayList<double[]>();
 				for (Feature feature : features) {
 							double[] des = feature.descriptor;
 							descriptors.add(des);
 		}
-				assignVisualWord(descriptors);
+				assignVisualWord(descriptors,label);
 
 	}
 
-	public static void assignVisualWord(ArrayList<double[]> feature_vector){
+	public static void assignVisualWord(ArrayList<double[]> feature_vector,Integer label){
 					Integer[] histogram  = new Integer[KmeansClustering.NUM_CLUSTERS];
+					Arrays.fill(histogram, new Integer(0));
 					Cluster[] clusters = kmeans.getClusters();
 					int word_index =0;
 					double min_distance =Double.MAX_VALUE; double temp;
@@ -107,12 +116,15 @@ public class MultiClassSVMclassification {
 						 		 		min_distance = temp;
 						 	 }
 					 }
+				//	 System.out.println("DEBUG: word_index"+word_index);
+				//	 System.out.println("DEBUG: word_index"+histogram.length);
 					 histogram[word_index]++;
 					 word_index= 0;
 					 min_distance = Double.MAX_VALUE;
 					 quantized_images.add(histogram);
 					}	
-					printUtility(quantized_images);
+					convertToLibSVMtrainData(histogram, label);
+					//printUtility(quantized_images);
 	}
 
 	public static void printUtility(ArrayList<Integer[]> hist){
@@ -124,9 +136,21 @@ public class MultiClassSVMclassification {
 						}
 	}
 	
-	public static void trainSVM(){
-			
+	public static void convertToLibSVMtrainData(Integer[] histogram, Integer label){
+					//now just append values to train data.
+					//once done , write to file to be safe.
+					int counter = 1;
+					Map<Integer, Integer> trainValue = new HashMap<Integer,Integer>();
+					for (Integer integer : histogram) {
+						trainValue.put(counter,integer);
+						counter++;
+					}
+						trainDataSVM.put(label, trainValue);
 	}
-
+	public static void writeLibSvmDataToFile(){
+					for(Entry<Integer, Map<Integer, Integer>> entrySet : trainDataSVM.entrySet()){
+										System.out.println(entrySet.getKey()+" "+entrySet.getValue().toString());
+					}
+	}
 
 }
